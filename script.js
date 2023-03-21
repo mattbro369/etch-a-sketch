91c750e04516d929d160c001270208b936f8ba46
Matt <ethacktry@gmail.com>
2023-03-21 21:03:26 +0100

    Button functionality completed. Need to now print grid according to input

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const getContainer = document.getElementById("container");
 const newDiv = document.createElement("div");
 const button = document.getElementById("button");
+let numOfSquares;
 
-// TODO Create function to take prompt value as number of squares each side of grid
-// TODO Check the number between 1 - 100, if not, repeat prompt.
+//TODO CSS Grid to layout according to numofsquares
 
 button.addEventListener("click", () => {
-  let num = prompt("Enter number of squares each side of grid: ");
-  let numOfSquares = parseInt(num);
-  if (numOfSquares < 1 || numOfSquares > 100) {
-    num = prompt("Enter a number between 1 - 100");
+  getNumOfSquares();
+  for (i = 0; i < numOfSquares; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.innerText = `${i + 1}`;
+    getContainer.appendChild(newDiv);
+    newDiv.classList.add("grid");
+
+    let grids = document.querySelectorAll(".grid");
+    for (let grid of grids) {
+      grid.addEventListener("mouseover", () => {
+        grid.classList.add("gridHover");
+      });
+    }
   }
-  return numOfSquares;
 });
 
-// Prints the grid
-for (i = 0; i < 256; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.innerText = `${i + 1}`;
-  getContainer.appendChild(newDiv);
-  newDiv.classList.add("grid");
-}
-
-// Adds the class on hover
-const grids = document.querySelectorAll(".grid");
-for (let grid of grids) {
-  grid.addEventListener("mouseover", () => {
-    grid.classList.add("gridHover");
-  });
+function getNumOfSquares() {
+  numOfSquares = parseInt(prompt("Enter number of squares each side of grid"));
+  while (numOfSquares < 1 || numOfSquares > 100) {
+    numOfSquares = parseInt(prompt("Enter a number between 1 - 100"));
+  }
+  numOfSquares *= numOfSquares;
+  return numOfSquares;
 }
