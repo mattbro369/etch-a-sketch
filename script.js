1331da5bd6ed33243e86588f82e287632b12d741
Matt <ethacktry@gmail.com>
2023-03-24 15:43:10 +0100

    Put some code into functions to make code drier. Set default grid to be 16 * 16

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
 const getContainer = document.getElementById("container");
-const newDiv = document.createElement("div");
+// const newDiv = document.createElement("div");
 const button = document.getElementById("button");
-let numOfSquares;
-let gridColNum;
+let numOfSquares = 16 * 16;
+let gridColNum = 16;
 
 //For the CSS Variables
 let htmlStyles = window.getComputedStyle(document.querySelector("html"));
 let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
 let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
 
+//Print the default grid of 16 x 16
+appendDivs();
+
 getContainer.addEventListener("mouseover", changeColour);
 
 button.addEventListener("click", () => {
-  if (numOfSquares !== undefined) {
-    clearGrid();
-  }
+  clearGrid();
   getNumOfSquares();
   calcGridColNum();
-  document.documentElement.style.setProperty("--colNum", gridColNum);
-  document.documentElement.style.setProperty("--rowNum", gridColNum);
-  for (i = 0; i < numOfSquares; i++) {
-    const newDiv = document.createElement("div");
-    // newDiv.innerText = `${i + 1}`;
-    getContainer.appendChild(newDiv);
-    newDiv.classList.add("grid-items");
-  }
+  appendDivs();
 });
 
 function getNumOfSquares() {
@@ -34,6 +28,16 @@ function getNumOfSquares() {
   }
   numOfSquares *= numOfSquares;
   return numOfSquares;
+}
+
+function appendDivs() {
+  for (i = 0; i < numOfSquares; i++) {
+    const newDiv = document.createElement("div");
+    getContainer.appendChild(newDiv);
+    newDiv.classList.add("grid-items");
+  }
+  document.documentElement.style.setProperty("--colNum", gridColNum);
+  document.documentElement.style.setProperty("--rowNum", gridColNum);
 }
 
 function changeColour(e) {
